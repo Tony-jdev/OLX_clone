31f7bb95a58b4b959bd89a38c4552918b1b9044b
Tony-jdev <artpopko28@gmail.com>
2024-03-16 20:07:00 +0200

    ApplicationDbContext added with Identity

---
+++ OLX_clone.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OLX_clone.Server.Data;
+
+public class ApplicationDbContext: IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options){}
+}
