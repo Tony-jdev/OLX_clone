f5d902f8d5c04b8f3423095be95f456d87af77a3
Tony-jdev <artpopko28@gmail.com>
2024-03-16 20:46:03 +0200

    Category model added and repository

--- OLX_clone.Server/Data/ApplicationDbContext.cs
+++ OLX_clone.Server/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OLX_clone.Server.Models;
 
 namespace OLX_clone.Server.Data;
 
 public class ApplicationDbContext: IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions options) : base(options){}
+    
+    public DbSet<Category> Categories { get; set; }
 }
