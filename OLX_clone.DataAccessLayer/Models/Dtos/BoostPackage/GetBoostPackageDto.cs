49332e73c225f5c76d1af84aae89698604173a83
Tony-jdev <artpopko28@gmail.com>
2024-07-09 23:13:37 +0300

    GetBoostPackageDto not needed data removed

--- OLX_clone.DataAccessLayer/Models/Dtos/BoostPackage/GetBoostPackageDto.cs
+++ OLX_clone.DataAccessLayer/Models/Dtos/BoostPackage/GetBoostPackageDto.cs
@@ -6,8 +6,6 @@ public class GetBoostPackageDto
     public string Name { get; set; }
     public double Price { get; set; }
     public int BoostCount { get; set; }
-    public string Type { get; set; }
-    public int NumberOfDays { get; set; }
     public int TopDurationInDays { get; set; }
     public int VipDurationInDays { get; set; }
 }
