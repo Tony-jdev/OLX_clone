3f834aa867f550d198bc94a461853baf1c466081
Ati4D <diominov228@gmail.com>
2024-07-10 14:12:12 +0300

    better service variant

--- olx_clone.client/src/Helpers/signalRServices.js
+++ olx_clone.client/src/Helpers/signalRServices.js
@@ -2,24 +2,17 @@ import { useState, useEffect, useRef } from 'react';
 import * as signalR from '@microsoft/signalr';
 
 export const useSignalR = (chatId) => {
-    const [connection, setConnection] = useState(null);
     const [messages, setMessages] = useState([]);
     const connectionRef = useRef(null);
 
     useEffect(() => {
         const connectToHub = async () => {
-            if (connectionRef.current) {
-                console.log('Already connected');
-                return;
-            }
-
             const newConnection = new signalR.HubConnectionBuilder()
                 .withUrl('https://localhost:5173/chathub', { transport: signalR.HttpTransportType.WebSockets })
                 .withAutomaticReconnect()
                 .build();
 
             connectionRef.current = newConnection;
-            setConnection(newConnection);
 
             newConnection.on('ReceiveMessage', (message) => {
                 setMessages((prevMessages) => [...prevMessages, message]);
@@ -27,14 +20,13 @@ export const useSignalR = (chatId) => {
 
             try {
                 await newConnection.start();
-                console.log('Connected to SignalR hub');
+                console.log('Connected!');
 
-                await newConnection.invoke('JoinChatGroup', chatId);
-                console.log('Joined chat group:', chatId);
+                await newConnection.invoke('JoinChatGroup', chatId)
+                    .then(() => console.log('Joined group'))
+                    .catch((e) => console.log('Failed to join group', e));
             } catch (e) {
                 console.log('Connection failed: ', e);
-                connectionRef.current = null;
-                setConnection(null);
             }
         };
 
@@ -44,11 +36,9 @@ export const useSignalR = (chatId) => {
 
         return () => {
             if (connectionRef.current) {
-                connectionRef.current.stop().then(() => {
-                    console.log('Disconnected from SignalR hub');
-                    connectionRef.current = null;
-                    setConnection(null);
-                });
+                connectionRef.current.stop();
+                connectionRef.current = null;
+                console.log('Disconnected');
             }
         };
     }, [chatId]);
